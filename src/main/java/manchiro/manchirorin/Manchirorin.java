54d0c87a4c026b37d49b01ad67b22ea58bef2127
BokuMohaimen <you10217114514@gmail.com>
2021-02-10 23:32:58 +0900

    mec

--- src/main/java/manchiro/manchirorin/Manchirorin.java
+++ src/main/java/manchiro/manchirorin/Manchirorin.java
@@ -9,6 +9,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jdk.nashorn.internal.runtime.JSType.isNumber;
+
 public final class Manchirorin extends JavaPlugin {
 
     boolean mch = false;
@@ -22,6 +24,10 @@ public final class Manchirorin extends JavaPlugin {
         getLogger().info("起動しました。");
         list = new ArrayList<>();
         vault = new VaultManager(this);
+        int max;
+        int jp;
+        int bet;
+        int hito;
     }
 
     @Override
@@ -31,57 +37,64 @@ public final class Manchirorin extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
         if (command.getName().equalsIgnoreCase("mch")) {
             if (args.length == 0) {
-                sender.sendMessage("§f==========" + prefix + "§f==========");
-                sender.sendMessage("§a§l/mch new §e§l[金額] §r: §f§l親としてマンチロを開始します");
-                sender.sendMessage("§a§l/mch join §r: §f§l子として開催中のマンチロに参加します");
-                sender.sendMessage("§a§l/mch rule §r: §f§lマンチロのルールを表示します");
+                p.sendMessage("§f==========" + prefix + "§f==========");
+                p.sendMessage("§a§l/mch new §e§l[金額] [人数]§r: §f§l親としてマンチロを開始します");
+                p.sendMessage("§a§l/mch join §r: §f§l子として開催中のマンチロに参加します");
+                p.sendMessage("§a§l/mch rule §r: §f§lマンチロのルールを表示します");
+                p.sendMessage("§d§lCreated by Mohaimen_Ksr");
                 return true;
             }
             if (args[0].equals("rule")) {
-                sender.sendMessage("§f==========" + prefix + "§f==========");
-                sender.sendMessage("§6役一覧: [1:1:1 jackpotチャンス] [それ以外の三つ揃い ゾロメ]");
-                sender.sendMessage("§6[出目合計10 man10] [1・2・3 イチ・ニ・サン・ﾀﾞｰ!!] [出目合計5 dan5]");
-                sender.sendMessage("§6役一覧: [二つそろって残りが・・ その数字が強さ]");
-                sender.sendMessage("");
-                sender.sendMessage("§e配当率: 『イチ・ニ・サン・ﾀﾞｰ!!:4倍勝(親のみ)』『ゾロメ:3倍勝』");
-                sender.sendMessage("§e『man10:2倍勝』『dan5:2倍負』 通常:1倍負/勝");
-                sender.sendMessage("§ejackpotの払い出し金額: 賭け金x10 or jackpotすべて のどちらか金額が低いほう");
+                p.sendMessage("§f==========" + prefix + "§f==========");
+                p.sendMessage("§6役一覧: [1:1:1 jackpotチャンス] [それ以外の三つ揃い ゾロメ]");
+                p.sendMessage("§6[出目合計10 man10] [1・2・3 イチ・ニ・サン・ﾀﾞｰ!!] [出目合計5 dan5]");
+                p.sendMessage("§6役一覧: [二つそろって残りが・・ その数字が強さ]");
+                p.sendMessage("");
+                p.sendMessage("§e配当率: 『イチ・ニ・サン・ﾀﾞｰ!!:4倍勝(親のみ)』『ゾロメ:3倍勝』");
+                p.sendMessage("§e『man10:2倍勝』『dan5:2倍負』 通常:1倍負/勝");
+                p.sendMessage("§ejackpotの払い出し金額: 賭け金x10 or jackpotすべて のどちらか金額が低いほう");
                 return true;
             }
             //new マンチロのゲームを開始↓
             if (args[0].equals("new")) {
+                int bet = Integer.parseInt(args[1]);
+                int hito = Integer.parseInt(args[2]);
+                if (args.length != 3) {
+                    p.sendMessage(prefix+"引数の数が違っています");
+                    return true;
+                }
                 if (mch = true) {
-                    sender.sendMessage(prefix + " 現在マンチロが開始されています");
+                    p.sendMessage(prefix + " 現在マンチロが開始されています");
                     return true;
                 }
-                double bit = -1;
-                int max = -1;
-                try {
-                    bit = Double.parseDouble(args[1]);
-                    max = Integer.parseInt(args[2]);
-                } catch (NumberFormatException e) {
-                    sender.sendMessage(prefix + " §c数字で入力してください");
+                if (!isNumber(args[1])) {
+                    p.sendMessage(prefix + " §c金額を数字で入力してください");
                     return true;
                 }
-                if (bit < 100000) {
-                    sender.sendMessage(prefix + " 掛け金は10万円以上で入力してください");
+                if (!isNumber(args[2])) {
+                    p.sendMessage(prefix + "§c人数を数字で入力してください");
+                    return true;
+                }
+                if (vault.getBalance(p.getUniqueId()) < bet * 5) {
+                    p.sendMessage(prefix + "§c必要金額に持っていません" + "§r" + bet * 5 "万円");
                     return true;
                 }
                 return true;
                 //join マンチロのゲームに参加↓
                 if (args[0].equals("join")) {
                     if (mch = false) {
-                        sender.sendMessage(prefix + " 現在マンチロは開催されていません");
+                        p.sendMessage(prefix + " 現在マンチロは開催されていません");
                         return true;
-                    } else if (max <= 0 || max >= 11) {
-                        sender.sendMessage(prefix + " 募集人数は1人以上10人未満で入力してください");
+                    } else if (hito <= 0 || hito >= 11) {
+                        p.sendMessage(prefix + " 募集人数は1人以上10人未満で入力してください");
                         return true;
                     }
                 } else {
-                    sender.sendMessage(prefix + " 使い方が間違っています");
-                    sender.sendMessage(prefix + " /mch と入力するとコマンド一覧が見れます");
+                    p.sendMessage(prefix + " 使い方が間違っています");
+                    p.sendMessage(prefix + " /mch と入力するとコマンド一覧が見れます");
                     return true;
                 }
             }
